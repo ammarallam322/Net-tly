0a2ef3a8841ba6e1aeb27794a542ece0da9b5693
Shereen Ahmed <Shereenahmed387@gmail.com>
2025-02-19 10:29:28 +0200

    governateV2

--- teamProject/Models/Governerate.cs
+++ teamProject/Models/Governerate.cs
@@ -1,23 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace teamProject.Models
 {
     public class Governerate
     {
         public int Id { get; set; }
-
+        [MinLength(3,ErrorMessage = "The number of characters must be greater than or equal to 3")]
+        
         public string Name { get; set; }
-
+        [MaxLength(4,ErrorMessage = "The number must be less than or equal to four")]
         public string Code { get; set; }
 
         //nav ??virtual?
-        public virtual List<Central> centrals { get; set; }
+        public virtual List<Central>? centrals { get; set; }
 
         //nav
-        public virtual Branch Branch { get; set; }
+        public virtual Branch? Branch { get; set; }
 
         [ForeignKey(nameof(Branch))]
-        public int Branch_Id { get; set; }
+        public int? Branch_Id { get; set; }
 
 
 
